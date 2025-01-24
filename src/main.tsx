7988fa1641d9151fe06b39b5ff422d9111aa81b3
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-01-23 18:23:58 -0800

    Linting

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('bodys')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
